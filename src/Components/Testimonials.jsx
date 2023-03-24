b41280ec27dfa07f433c54a1ffdc8ecb1c03be2e
jayceovaire <118041687+jayceovaire@users.noreply.github.com>
2023-03-24 18:45:58 -0400

    Week 1 Commit 1.2

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -1,51 +1,49 @@
 import './Testimonials.css'
 function Testimonials() {
 
-    let styles = {
-        background: '#d5d5d5'
-    }
+
 
     return (
         <>
             <div className={'tcontainer'}>
                 <div className={"testimonial"}>
-                    <p style={styles}>"The food was amazing! The atmosphere was great too!"</p>
+                    <p className={'tquote'}>"The food was amazing! The atmosphere was great too!"</p>
                     <div className={'starbox'}>
-                        <div style={styles}>*</div>
-                        <div style={styles}>*</div>
-                        <div style={styles}>*</div>
-                        <div style={styles}>*</div>
-                        <div style={styles}>*</div>
+                        <div className={'star'}>*</div>
+                        <div className={'star'}>*</div>
+                        <div className={'star'}>*</div>
+                        <div className={'star'}>*</div>
+                        <div className={'star'}>*</div>
                     </div>
                 </div>
                 <div className={"testimonial"}>
-                    <p style={styles}>"The food was amazing! The atmosphere was great too!"</p>
+                    <p className={'tquote'}>"The food was amazing! The atmosphere was great too!"</p>
                     <div className={'starbox'}>
-                        <div style={styles}>*</div>
-                        <div style={styles}>*</div>
-                        <div style={styles}>*</div>
-                        <div style={styles}>*</div>
-                        <div style={styles}>*</div>
+                        <div className={'star'}>*</div>
+                        <div className={'star'}>*</div>
+                        <div className={'star'}>*</div>
+                        <div className={'star'}>*</div>
+                        <div className={'star'}>*</div>
                     </div>
                 </div>
                 <div className={"testimonial"}>
-                    <p style={styles}>"The food was amazing! The atmosphere was great too!"</p>
+                    <p className={'tquote'}>"The food was amazing! The atmosphere was great too!"</p>
                     <div className={'starbox'}>
-                        <div style={styles}>*</div>
-                        <div style={styles}>*</div>
-                        <div style={styles}>*</div>
-                        <div style={styles}>*</div>
-                        <div style={styles}>*</div>
+                        <div className={'star'}>*</div>
+                        <div className={'star'}>*</div>
+                        <div className={'star'}>*</div>
+                        <div className={'star'}>*</div>
+                        <div className={'star'}>*</div>
                     </div>
                 </div>
                 <div className={"testimonial"}>
-                    <p style={styles}>"The food was amazing! The atmosphere was great too!"</p>
+                    <p className={'tquote'}>"The food was amazing! The atmosphere was great too!"</p>
                     <div className={'starbox'}>
-                        <div style={styles}>*</div>
-                        <div style={styles}>*</div>
-                        <div style={styles}>*</div>
-                        <div style={styles}>*</div>
-                        <div style={styles}>*</div>
+                        <div className={'star'}>*</div>
+                        <div className={'star'}>*</div>
+                        <div className={'star'}>*</div>
+                        <div className={'star'}>*</div>
+                        <div className={'star'}>*</div>
                     </div>
                 </div>
 
